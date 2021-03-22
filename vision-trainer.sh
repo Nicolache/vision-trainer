9b6ebe4d6f0905bce76d04800d30bd5695d10d6d
Nicolache <soundriver@mail.ru>
2021-03-22 11:57:54 +0300

    Update vision-trainer.sh

--- vision-trainer.sh
+++ vision-trainer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cat text.txt
-timer=120
+interval=120
 while true
 do
   cols=`tput cols`
   lines=`tput lines`
   tput cup $(( ( RANDOM % $lines )  + 1 )) $(( ( RANDOM % $lines )  + 1 ))
-  sleep $timer
+  sleep $interval
 done
 
